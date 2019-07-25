4b3ed9f74417aaddcd0d424376ccd9e8d282f0bc
James Klein <kleinjm007@gmail.com>
2019-07-25 02:19:20 -0400

    break test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.describe User do
   describe "validations and relations" do
     it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to_not validate_presence_of(:last_name) }
   end
 end
